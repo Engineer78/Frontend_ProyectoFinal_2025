9428d217ea7c56889783c81913390be31bc59202
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 17:37:59 -0500

    Se crea el componente LoginForm.jsx y se ajusta de function a const.

---
+++ src/components/moduloInventario/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+/// Este componente es un formulario de inicio de sesión
+/// que permite a los usuarios ingresar su nombre de usuario y contraseña.
+const LoginForm = () => {
+  return (
+    <div>LoginForm</div>
+  )
+}
+
+export default LoginForm
