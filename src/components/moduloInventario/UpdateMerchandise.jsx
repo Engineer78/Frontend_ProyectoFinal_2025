c7247e3df05bacaa03be844836fa30bea940e50e
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-15 16:15:10 -0500

    Se crea el componente UpdateMerchandise.jsx para actualizar información de mercancía

---
+++ src/components/moduloInventario/UpdateMerchandise.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import styles from "../styles/updatemerchandise.module.css";
+import SaveIcon from "@mui/icons-material/Save";
+import CleaningServicesIcon from "@mui/icons-material/CleaningServices";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import SearchIcon from "@mui/icons-material/Search";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import { Link } from "react-router-dom";
+import axios from "axios"; // Importa axios
+import { number } from "yup";
