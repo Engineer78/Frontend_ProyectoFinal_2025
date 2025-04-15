1b1ee53162cb505154247d0b79d18f6d52803fc5
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-15 18:31:37 -0500

    Se se crea el componente MerchandiseQuery.jsx con sus respectivas importaciones

---
+++ src/components/moduloInventario/MerchandiseQuery.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import Header from './Header';
+import styles from '../styles/merchandisequery.module.css';
+import { Link } from 'react-router-dom';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import SearchIcon from '@mui/icons-material/Search';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import axios from 'axios';
