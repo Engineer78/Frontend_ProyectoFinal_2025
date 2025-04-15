bc03f2677559aa3bc38ce62049b5032a9c91c369
EliteBoku <devijunior3223@gmail.com>
2025-04-15 17:20:40 -0500

    Se estructura la base del componente Header con manejo de props y lógica de menú inicial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from '../styles/header.module.css';
-import logo from '../assets/Logo Sin Fondo_FFIG.png';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "../styles/header.module.css";
+import logo from "../assets/Logo Sin Fondo_FFIG.png";
 
+const Header = ({ title, subtitle, showLogo = true, showHelp = true }) => {
+  console.log("Props recibidas en Header:", {
+    title,
+    subtitle,
+    showLogo,
+    showHelp,
+  });
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  console.log("Props recibidas en Header:", {
+    title,
+    subtitle,
+    showLogo,
+    showHelp,
+  }); // Para verificar las props
+  return <header></header>;
+};
+
+export default Header;
