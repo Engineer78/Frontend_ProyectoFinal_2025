fa867605f81cbe2a3aea1a1a60add669a08ae600
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-15 18:35:55 -0500

    Se crea el componente principal del módulo de consulta de productos

--- src/components/moduloInventario/MerchandiseQuery.jsx
+++ src/components/moduloInventario/MerchandiseQuery.jsx
@@ -11,3 +11,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: 'http://localhost:8080/api'
 });
+
+// Componente principal del módulo de consulta de productos
+const MerchandiseQuery = () => {
+}
