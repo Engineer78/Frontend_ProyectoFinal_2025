9256490c718bc5a449f7c501cf63fa4760a3c26c
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 17:53:05 -0500

    Se renderiza el componente usando html, aquí se crean los campos con inputs para el usuario y la contraseña al igual que el botón de ingresar.

--- src/components/moduloInventario/LoginForm.jsx
+++ src/components/moduloInventario/LoginForm.jsx
@@ -35,9 +35,56 @@ const LoginForm = () => {
         }
       };
 
-    return (
-        <div>LoginForm</div>
-    )
-}
-
-export default LoginForm
+      // Este componente renderiza un formulario de inicio de sesión
+      // que incluye campos para el nombre de usuario y la contraseña,
+      return (
+        <div className={styles["form-wrapper"]}>
+          <form className={styles["login-form"]} onSubmit={handleLogin}>
+            <h2>Inicio de sesión</h2>
+            <p>Por favor loguearse para continuar.</p>
+    
+            <label htmlFor="username">Usuario</label>
+            <input
+              type="email"
+              id="username"
+              placeholder="jujo_systemsoft@gmail.com"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} 
+            />
+    
+            <label htmlFor="password">Contraseña</label>
+            <div className={styles["password-container"]}>
+              <input
+                type={passwordVisible ? "text" : "password"}
+                id="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
+              />
+              <span
+                className={styles["toggle-password"]}
+                onClick={togglePasswordVisibility}
+                title={passwordVisible ? "Ocultar contraseña" : "Mostrar contraseña"}
+              >
+                <span className="material-icons">
+                  {passwordVisible ? "visibility_off" : "visibility"}
+                </span>
+              </span>
+            </div>
+    
+            <button type="submit" className={styles["submit-button"]}>
+              Ingresar
+              <span className="material-icons" style={{ marginLeft: "0.5rem" }}>
+                login
+              </span>
+            </button>
+    
+            <a href="https://www.gmail.com" className={styles["forgot-password"]}>
+              ¿Olvidó su contraseña? <span>Clic aquí</span> para recuperarla.
+            </a>
+          </form>
+        </div>
+      );
+    };
+    
+    export default LoginForm;
