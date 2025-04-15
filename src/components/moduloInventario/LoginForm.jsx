f384d29de76580e4f556de9a7f6fddaa35775436
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 17:43:38 -0500

    Se define los estados para el nombre de usuario, la constraseña y la visibilidad de la contraseña.

--- src/components/moduloInventario/LoginForm.jsx
+++ src/components/moduloInventario/LoginForm.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../styles/loginform.module.css';
-import "material-icons/iconfont/material-icons.css"; 
+import "material-icons/iconfont/material-icons.css";
 
 /// Este componente es un formulario de inicio de sesión
 /// que permite a los usuarios ingresar su nombre de usuario y contraseña.
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
+    
+    // se definen los estados para el nombre de usuario y la contraseña
+    // y la visibilidad de la contraseña
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    return (
+        <div>LoginForm</div>
+    )
 }
 
 export default LoginForm
