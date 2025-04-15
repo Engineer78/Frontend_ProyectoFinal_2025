8f9557910cfb8980b6f47683cd166dc2bec776f1
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 17:41:01 -0500

    Se crean los imports necesarios para el componente.

--- src/components/moduloInventario/LoginForm.jsx
+++ src/components/moduloInventario/LoginForm.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/loginform.module.css';
+import "material-icons/iconfont/material-icons.css"; 
 
 /// Este componente es un formulario de inicio de sesión
 /// que permite a los usuarios ingresar su nombre de usuario y contraseña.
