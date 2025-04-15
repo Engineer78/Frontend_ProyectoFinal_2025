6177fc24c16f8aa969982751924a4271b77b5f69
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-15 16:21:47 -0500

    Se crea initialize state variables for product and supplier data in UpdateMerchandise component

--- src/components/moduloInventario/UpdateMerchandise.jsx
+++ src/components/moduloInventario/UpdateMerchandise.jsx
@@ -13,3 +13,25 @@ import { number } from "yup";
 const api = axios.create({ // Crea una instancia de axios
     baseURL: 'http://localhost:8080/api'
 });
+
+const UpdateMerchandise = () => {
+    const [supplierId, setSupplierId] = useState("");
+    const [supplierName, setSupplierName] = useState("");
+    const [supplierNIT, setSupplierNIT] = useState("");
+    const [supplierPhone, setSupplierPhone] = useState("");
+    const [supplierAddress, setSupplierAddress] = useState("");
+    const [productCategory, setProductCategory] = useState("");
+    const [suppliers, setSuppliers] = useState([]);
+    const [productCode, setProductCode] = useState("");
+    const [productName, setProductName] = useState("");
+    const [productQuantity, setProductQuantity] = useState("");
+    const [unitValue, setUnitValue] = useState("");
+    const [totalValue, setTotalValue] = useState("");
+    const [productImage, setProductImage] = useState(null);
+    const [productImageUrl, setProductImageUrl] = useState("");
+    const [originalProductIndex, setOriginalProductIndex] = useState(-1);
+    const [activeTab, setActiveTab] = useState('actualizar');
+    const [selectedCategoryId, setSelectedCategoryId] = useState(""); // ID de la categoría seleccionada
+    const [categories, setCategories] = useState([]); // Lista completa de categorías
+    const [isCodeDisabled, setIsCodeDisabled] = useState(false);
+}  
