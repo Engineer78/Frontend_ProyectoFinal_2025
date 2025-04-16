4a7633abdc5930cc429c59c6a647fa223b500693
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-15 19:40:02 -0500

    Se implementa función handleLogout para cerrar sesión y redirigir al login

--- src/components/moduloInventario/MenuPcpal.jsx
+++ src/components/moduloInventario/MenuPcpal.jsx
@@ -11,3 +11,9 @@ const storedUser = JSON.parse(localStorage.getItem('user'));
 // Verifica si el usuario tiene permisos para acceder al módulo de usuarios
 // Solo usuarios distintos a 'admin@gmail.com' están autorizados
 const isUserAllowed = storedUser && storedUser.username !== 'admin@gmail.com';
+
+// Función para cerrar sesión del usuario
+const handleLogout = () => {
+    localStorage.removeItem('user'); // Elimina al usuario del localStorage al hacer logout
+    navigate('/login'); // Redirige a la pantalla de login sin recargar la página
+    };
