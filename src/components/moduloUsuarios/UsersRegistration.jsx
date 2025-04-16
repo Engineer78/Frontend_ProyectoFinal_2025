2dadc1147646d0609f6127060484a90358e90474
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-16 00:48:57 -0500

    Se ajusta el import para la hoja de estilos userregistration.module.css.

--- src/components/moduloUsuarios/UsersRegistration.jsx
+++ src/components/moduloUsuarios/UsersRegistration.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Header from "../Header";
-import styles from "../../styles/inventoryregistration.module.css";
+import styles from "../../styles/usersRegistration.module.css";
 import SaveIcon from '@mui/icons-material/Save';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import UploadFileIcon from '@mui/icons-material/UploadFile';
+
 
 // se crea el componente UsersRegistration
 const InventoryRegistration = () => {
