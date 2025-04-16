86bf1105b0c29c80a1b695b5e4596e83ba953238
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 22:52:13 -0500

    Se agregan los imports necesesarios al código, estos se reutilizan del componente InventoryRegistration.jsx

--- src/components/moduloUsuarios/UsersRegistration.jsx
+++ src/components/moduloUsuarios/UsersRegistration.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import Header from "../Header";
+import styles from "../../styles/inventoryregistration.module.css";
+import SaveIcon from '@mui/icons-material/Save';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
 
 // se crea el componente UsersRegistration
 const InventoryRegistration = () => {
