c074dbeb2207f1a730808e911ca860fe138b030b
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 20:56:23 -0500

    Se corrigen los imports para el componente Header.jsx y para la hoja de estilos inventoryregistration.module.css dentro del mismo componente InventoryRegistration.jsx

--- src/components/moduloInventario/InventoryRegistration.jsx
+++ src/components/moduloInventario/InventoryRegistration.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-import Header from "./Header";
-import styles from "../styles/inventoryregistration.module.css";
+import Header from "../Header";
+import styles from "../../styles/inventoryregistration.module.css";
 import SaveIcon from '@mui/icons-material/Save';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
