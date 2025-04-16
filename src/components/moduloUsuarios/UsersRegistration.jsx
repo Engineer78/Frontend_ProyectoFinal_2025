786905909ab46927a46435f6314ead4ca7925b39
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 23:48:59 -0500

    Se utiliza el html del componente InventoryRegistration.jsx refactorizandolo para usarlo en UsersRegistration.jsx

--- src/components/moduloUsuarios/UsersRegistration.jsx
+++ src/components/moduloUsuarios/UsersRegistration.jsx
@@ -9,9 +9,213 @@ import UploadFileIcon from '@mui/icons-material/UploadFile';
 
 // se crea el componente UsersRegistration
 const InventoryRegistration = () => {
-  return (
-    <div>UsersRegistration</div>
-  )
+
+
+    // Se renderiza el componente
+    return (
+        <>
+            <Header
+                title="Módulo registro de usuarios"
+                subtitle="Hardware Store Inventory FFIG"
+                showLogo={true}
+                showHelp={true}
+            />
+
+            {/* Pestañas debajo del header */}
+            <div className={styles.tabs}>
+                <Link
+                    to="/users-registration"
+                    className={`${styles.tabButton} ${activeTab === "registro" ? styles.active : ""}`}
+                    onClick={() => handleTabClick("registro")}
+                >
+                    Registro de Usuarios
+                </Link>
+
+                {/*<Link
+                    to="/users-query"
+                    className={`${styles.tabButton} ${activeTab === "consulta" ? styles.active : ""}`}
+                    onClick={() => handleTabClick("consulta")}
+                >
+                    Consulta de Usuarios
+                </Link>
+
+                <Link
+                    to="/update-users"
+                    className={`${styles.tabButton} ${activeTab === "actualizar" ? styles.active : ""}`}
+                    onClick={() => handleTabClick("actualizar")}
+                >
+                    Actualizar Usuario
+                </Link>
+
+                <Link
+                    to="/delete-users"
+                    className={`${styles.tabButton} ${activeTab === "eliminar" ? styles.active : ""}`}
+                    onClick={() => handleTabClick("eliminar")}
+                >
+                    Eliminar Usuarios
+                </Link>*/}
+            </div>
+
+            {/* Contenido dependiendo de la pestaña activa */}
+            {activeTab === "registro" && (
+                <div className={styles.container}>
+                    <h2 className={styles.title}>
+                        Ingrese la información solicitada para crear el registro
+                    </h2>
+
+                    <div className={styles.formContainer}>
+                        <form className={styles.formLeft}>
+                            <label className={styles.inputLabel}>Número de Documento:</label>
+                            <input
+                                type="text"
+                                placeholder="Número de Documentor (Obligatorio)"
+                                value={userID}
+                                onChange={(e) => setUserID(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Nombre(s):</label>
+                            <input
+                                type="text"
+                                placeholder="Nombre(s) (Obligatorio)"
+                                value={userName}
+                                onChange={(e) => setUserNames(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Primer Apellido:</label>
+                            <input
+                                type="text"
+                                placeholder="Primer Apellido (Obligatorio)"
+                                value={userLastName}
+                                onChange={(e) => setUserLastName(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Segundo Apellido :</label>
+                            <input
+                                type="text"
+                                placeholder="Segundo Apellido (Opcional)"
+                                value={userSecondLastName}
+                                onChange={(e) => setUserSecondLastName(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Nombre de Usuario:</label>
+                            <input
+                                type="text"
+                                placeholder="Nombre de Usuario (Obligatorio)"
+                                value={userAlias}
+                                onChange={(e) => setUserAlias(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Contraseña:</label>
+                            <input
+                                type="text"
+                                placeholder="Contraseña (Obligatorio)"
+                                value={userPassword}
+                                onChange={(e) => setUserPassword(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Teléfono Móvil:</label>
+                            <input
+                                type="number"
+                                placeholder="Teléfono Móvil (Obligatorio)"
+                                value={userPhone}
+                                onChange={(e) => setUserPhone(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Dirección de Residencia:</label>
+                            <input
+                                type="text"
+                                placeholder="Dirección de Residencia (Obligatorio)"
+                                value={userAddress}
+                                onChange={(e) => setUserAddress(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+
+                            <label className={styles.inputLabel}>Contacto de Emergencia:</label>
+                            <input
+                                type="number"
+                                placeholder="Contacto de Emergencia (Obligatorio)"
+                                value={userEmergencyContact}
+                                onChange={(e) => setUserEmergencyContact(e.target.value)}
+                                required
+                                className={styles.inputValorTotal}
+                            />
+
+                            <label className={styles.inputLabel}>Teléfono de Contacto:</label>
+                            <input
+                                type="text"
+                                placeholder="Teléfono de Contacto (Obligatorio)"
+                                value={userContactPhone}
+                                onChange={(e) => setUserContactPhone(e.target.value)}
+                                required
+                                className={styles.input}
+                            />
+                        </form>
+
+                        <form className={styles.formRight}>
+                            <div className={styles.selectGroup}>
+                                <div className={styles.formGroup}>
+                                    <label className={styles.inputLabel}>Tipo de Documento</label>
+                                    <div className={styles.selectWrapper}>
+                                        <select id="tipoDocumento" className={styles.select} required>
+                                            <option value=""> Seleccione un tipo de documento </option>
+                                            <option value="CC">Cédula de Ciudadanía (CC)</option>
+                                            <option value="CE">Cédula de Extranjería (CE)</option>
+                                            <option value="PAS">Pasaporte (PA)</option>
+                                            <option value="TI">Tarjeta de Identidad (TI)</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className={styles.formGroup}>
+                                    <label className={styles.inputLabel}>Tipo de Rol</label>
+                                    <div className={styles.selectWrapper}>
+                                        <select id="tipoRol" className={styles.select} required>
+                                            <option value=""> Seleccione un rol </option>
+                                            <option value="administrador">Administrador</option>
+                                            <option value="almacenista">Almacenista</option>
+                                            <option value="propietario">Propietario</option>
+                                            <option value="vendedor">Vendedor</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div className={styles.buttons}>
+                        <button type="button" onClick={handleSave} className={styles.button}>
+                            Guardar <SaveIcon />
+                        </button>
+                        <button type="button" onClick={handleClear} className={styles.button}>
+                            Limpiar <CleaningServicesIcon />
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => (window.location.href = "/menu-principal")}
+                            className={styles.button}
+                        >
+                            Salir <ExitToAppIcon style={{ marginLeft: 8 }} />
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default UsersRegistration
