78fb0cdc99d3a067001a7c5e490a389279a5ad8e
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 22:36:22 -0500

    Se refactoriza el código del menú principal, habilitando la pestaña para registro de usuarios.

--- src/components/MenuPcpal.jsx
+++ src/components/MenuPcpal.jsx
@@ -17,14 +17,14 @@ const MenuPcpal = () => {
     // Función para cerrar sesión del usuario
     const handleLogout = () => {
         localStorage.removeItem('user'); // Elimina al usuario del localStorage al hacer logout
-        navigate('/login'); // Redirige a la pantalla de login sin recargar la página
-        };
+        navigate('/login'); // Navega a la pantalla de login sin recargar la página
+    };
 
     // Renderiza el menú principal con encabezado, botones de navegación y botón de salida.
-  // El botón de registro de usuarios está deshabilitado si el usuario es admin.
-  // Incluye enlaces a los módulos de registro de usuarios e inventario.
+    // El botón de registro de usuarios está deshabilitado si el usuario es admin.
+    // Incluye enlaces a los módulos de registro de usuarios e inventario.
     return (
-            <div>
+        <div>
             <Header
                 title="Menú Principal"
                 subtitle="Hardware Store Inventory FFIG"
@@ -33,33 +33,38 @@ const MenuPcpal = () => {
             />
             <div className={styles['menu-principal']}>
                 <div className={styles['menu-options']}>
-                {/* Botón condicional (habilitado/deshabilitado) */}
-                <Link
-                    to={isUserAllowed ? '/user-registration' : '#'}
-                    className={`${styles['menu-button']} ${!isUserAllowed ? styles['disabled'] : ''}`}
-                    onClick={(e) => {
-                    if (!isUserAllowed) e.preventDefault(); // Evita la navegación si está deshabilitado
-                    }}
-                >
-                    Módulo registro usuarios
-                    <span className="material-icons">person</span>
-                </Link>
+                    {/* Botón condicional (habilitado/deshabilitado) */}
+                    {/*<Link
+                        to={isUserAllowed ? '/user-registration' : '#'}
+                        className={`${styles['menu-button']} ${!isUserAllowed ? styles['disabled'] : ''}`}
+                        onClick={(e) => {
+                            if (!isUserAllowed) e.preventDefault(); // Evita la navegación si está deshabilitado
+                        }}
+                    >
+                        Módulo registro usuarios
+                        <span className="material-icons">person</span>
+                    </Link>*/}
+                    {/* Botón habilitado */}
+                    <Link to="/users-registration" className={styles['menu-button']}>
+                        Módulo registro usuarios
+                        <span className="material-icons">person</span>
+                    </Link>
 
-                {/* Botón habilitado */}
-                <Link to="/inventory-registration" className={styles['menu-button']}>
-                    Módulo registro inventario
-                    <span className="material-icons">description</span>
-                </Link>
+                    {/* Botón habilitado */}
+                    <Link to="/inventory-registration" className={styles['menu-button']}>
+                        Módulo registro inventario
+                        <span className="material-icons">description</span>
+                    </Link>
                 </div>
 
                 <div className={styles['logout-button-container']}>
-                <button
-                    className={styles['logout-button']}
-                    onClick={handleLogout} // Llama a handleLogout para hacer logout
-                >
-                    Salir
-                    <span className="material-icons">exit_to_app</span>
-                </button>
+                    <button
+                        className={styles['logout-button']}
+                        onClick={handleLogout} // Llama a handleLogout para hacer logout
+                    >
+                        Salir
+                        <span className="material-icons">exit_to_app</span>
+                    </button>
                 </div>
             </div>
         </div>
