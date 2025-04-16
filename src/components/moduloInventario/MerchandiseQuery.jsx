c961d4c700d59eb0b811cd5e153013c14ef2e0bb
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 20:54:32 -0500

    Se corrigen los imports para el componente Header.jsx y para la hoja de estilos merchandisequery.module.css dentro del mismo componente MerchandiseQuery.jsx

--- src/components/moduloInventario/MerchandiseQuery.jsx
+++ src/components/moduloInventario/MerchandiseQuery.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import Header from './Header';
-import styles from '../styles/merchandisequery.module.css';
+import Header from '../Header';
+import styles from '../../styles/merchandisequery.module.css';
 import { Link } from 'react-router-dom';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import SearchIcon from '@mui/icons-material/Search';
