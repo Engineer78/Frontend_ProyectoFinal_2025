7d4b894d99b076b8970f043d4c9f93fa75d38d89
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-15 22:50:15 -0500

    Se crea el componente UsersRegistration.jsx, usando el snippet rfce y se cambiala function por const flecha.

---
+++ src/components/moduloUsuarios/UsersRegistration.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// se crea el componente UsersRegistration
+const InventoryRegistration = () => {
+  return (
+    <div>UsersRegistration</div>
+  )
+}
+
+export default UsersRegistration
