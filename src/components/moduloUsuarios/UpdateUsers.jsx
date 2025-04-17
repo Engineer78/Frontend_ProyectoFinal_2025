36e524b4755475ad8144a76cc85fe3ab7142176d
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-17 18:48:20 -0500

    Se agregan los imports respectivos para el uso en el componente UpdateUsers.jsx

--- src/components/moduloUsuarios/UpdateUsers.jsx
+++ src/components/moduloUsuarios/UpdateUsers.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import Header from "../Header";
+import styles from "../../styles/usersRegistration.module.css";
+import SaveIcon from '@mui/icons-material/Save';
+import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 const UpdateUsers = () => {
 
