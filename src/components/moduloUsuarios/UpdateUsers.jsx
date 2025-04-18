df4d54ba0ab946814d8253112ca1a166bf3a20be
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-17 20:43:43 -0500

    Se agrega el import para el manejo del icono del botón buscar.

--- src/components/moduloUsuarios/UpdateUsers.jsx
+++ src/components/moduloUsuarios/UpdateUsers.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Header from "../Header";
 import styles from "../../styles/UpdateUsers.module.css";
 import SaveIcon from '@mui/icons-material/Save';
+import SearchIcon from '@mui/icons-material/Search';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
