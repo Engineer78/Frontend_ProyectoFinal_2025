ff9acd1e83715d212bf0d18afcda938723fba951
Juan Gallego <homertrixmemo@yahoo.com>
2025-04-19 00:12:13 -0500

    Se actualiza el import para el icono del botón Guardar

--- src/components/moduloInventario/InventoryRegistration.jsx
+++ src/components/moduloInventario/InventoryRegistration.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Header from "../Header";
 import styles from "../../styles/inventoryregistration.module.css";
-import SaveIcon from '@mui/icons-material/Save';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
