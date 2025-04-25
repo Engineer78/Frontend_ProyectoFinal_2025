6bdc6c736d1fb9629fc53c621289af28980fd2c0
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-25 18:50:39 -0500

    Luego de hacer unas pruebas se decide dejar la función para obtener iniciales fuera del componente principal UserBadge.jsx

--- src/components/UserBadge.jsx
+++ src/components/UserBadge.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import '../style/UserBadge.css';
+import { getLogerUserData} from '../authUtils';
 
 
 const UserBadge = ({ nombreCompleto, rol }) => {
-
-    // Función para obtener las iniciales del nombre completo
-    // Separa el nombre completo en partes y toma la primera letra del primer nombre y la primera letra del primer apellido
-    // Si no hay nombre o apellido, devuelve una cadena vacía
-    export const obtenerIniciales = (nombres, primerApellido) => {
-        const inicialNombre = nombres?.trim().split(' ')[0]?.[0]?.toUpperCase() || '';
-        const inicialApellido = primerApellido?.trim()[0]?.toUpperCase() || '';
-        return inicialNombre + inicialApellido;
-      };
 
     return (
         <div className="user-badge">
