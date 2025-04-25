4d17a1e749db518d838db4f3df12b0a471e05384
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-25 18:16:15 -0500

    Se modifica la función para obtener el nombre completo, para obener la primera inicial del primer nombre y la inicial del primer apellido dejando listo para traer de la base de datos.

--- src/components/UserBadge.jsx
+++ src/components/UserBadge.jsx
@@ -7,13 +7,11 @@ const UserBadge = ({ nombreCompleto, rol }) => {
     // Función para obtener las iniciales del nombre completo
     // Separa el nombre completo en partes y toma la primera letra del primer nombre y la primera letra del primer apellido
     // Si no hay nombre o apellido, devuelve una cadena vacía
-    const obtenerIniciales = (nombreCompleto) => {
-        const partes = nombreCompleto.trim().split(' ');
-
-        const inicialNombre = partes[0]?.[0]?.toUpperCase() || '';
-        const inicialApellido = partes[1]?.[0]?.toUpperCase() || '';
-
+    export const obtenerIniciales = (nombres, primerApellido) => {
+        const inicialNombre = nombres?.trim().split(' ')[0]?.[0]?.toUpperCase() || '';
+        const inicialApellido = primerApellido?.trim()[0]?.toUpperCase() || '';
         return inicialNombre + inicialApellido;
+      };
     };
 
     return (
