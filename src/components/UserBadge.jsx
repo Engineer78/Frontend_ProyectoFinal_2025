7a0a15ecf4d45234147f9295e968020dbc0891de
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-25 17:38:06 -0500

    Se crea el componente UserBadge.jsx usando el snippet rfce y se configura la función principal como const para mostrar la insignia a futuro el perfil del usuario dentro de la interfaz del software.

---
+++ src/components/UserBadge.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/UserBadge.css';
+
+
+const UserBadge = ({nombreCompleto, rol}) => {
+  return (
+    <div>UserBadge</div>
+  )
+}
+
+export default UserBadge
