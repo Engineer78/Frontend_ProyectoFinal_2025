fe704e2618a38a422bbc2ecd3e8a2646f5bb97a4
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-25 17:44:32 -0500

    Se crea la función obtenerIniciales la cual toma la primera letra del primer nombre y la primera letra del primer apellido, de no encontrar nombre o palleido devuelve una cadena vacía.

--- src/components/UserBadge.jsx
+++ src/components/UserBadge.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import '../style/UserBadge.css';
 
 
-const UserBadge = ({nombreCompleto, rol}) => {
-  return (
-    <div>UserBadge</div>
-  )
+const UserBadge = ({ nombreCompleto, rol }) => {
+
+    // Función para obtener las iniciales del nombre completo
+    // Separa el nombre completo en partes y toma la primera letra del primer nombre y la primera letra del primer apellido
+    // Si no hay nombre o apellido, devuelve una cadena vacía
+    const obtenerIniciales = (nombreCompleto) => {
+        const partes = nombreCompleto.trim().split(' ');
+
+        const inicialNombre = partes[0]?.[0]?.toUpperCase() || '';
+        const inicialApellido = partes[1]?.[0]?.toUpperCase() || '';
+
+        return inicialNombre + inicialApellido;
+    };
+
+    return (
+        <div>UserBadge</div>
+    )
 }
 
 export default UserBadge
