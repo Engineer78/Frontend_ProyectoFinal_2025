3250462066ca04280b90b25a6df1b3018027fbfa
Joaquín Jiménez <jjimenezr78@gmail.com>
2025-04-28 03:55:10 -0500

    Fix: se corrige el select  tipoDocuemnto especificamente el <option> ya que solo estaba mostrando el código y no el nombre del tipo de documento en el componente UsersRegistration.jsx

--- src/components/moduloUsuarios/UsersRegistration.jsx
+++ src/components/moduloUsuarios/UsersRegistration.jsx
@@ -364,8 +364,8 @@ const UsersRegistration = () => {
                                         >
                                             <option value=""> Seleccione un Tipo de Documento </option>
                                             {documentTypes.map((tipo) => (
-                                                <option key={tipo.idTipoDocumento} value={tipo.idTipoDocumento}>
-                                                    {tipo.nombreTipoDocumento} ({tipo.codigo})
+                                                <option key={tipo.idTipoDocumento} value={tipo.codigo}>
+                                                    {tipo.nombre} - ({tipo.codigo})
                                                 </option>
                                             ))}
                                         </select>
